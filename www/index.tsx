0ed3aa3f58e1693ca783706789c3d97caee605cb
i.kalashnikov <i.kalashnikov@corp.mail.ru>
2022-05-10 00:32:19 +0700

    Migrate to Vite

--- www/index.tsx
+++ www/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from '../components/app';
+import App from './src/components/app';
+import './index.css'
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App/>);
