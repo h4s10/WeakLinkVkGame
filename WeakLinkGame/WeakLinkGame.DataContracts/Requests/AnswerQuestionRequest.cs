88ebabd04dd27b8aabc0acd14d04e03b6f11c8c7
lapinosore <gudkovpavel@icloud.com>
2022-05-15 19:16:24 +0700

    Added pass question logic

--- WeakLinkGame/WeakLinkGame.DataContracts/Requests/AnswerQuestionRequest.cs
+++ WeakLinkGame/WeakLinkGame.DataContracts/Requests/AnswerQuestionRequest.cs
@@ -2,6 +2,7 @@ namespace WeakLinkGame.DataContracts.Requests;
 
 public class AnswerQuestionRequest
 {
+    public bool IsPass { get; set; }
     public bool IsBank { get; set; }
     public int? BankSum { get; set; }
     public int? QuestionId { get; set; }
