b49ffd70087a87d3e0922c5482a1e34a8eb622ce
a.yunyshev <a.yunyshev@corp.vk.com>
2022-05-16 02:52:45 +0700

    Fix iPad score

--- www/src/components/Score/index.tsx
+++ www/src/components/Score/index.tsx
@@ -9,11 +9,11 @@ const COLS = 8;
 const ROWS = 32;
 
 const Score: FC<Props> = ({ power = 0 }) => {
-  return <div className="grid grid-rows-32 grid-cols-8 gap-[0.3rem] place-content-around w-fit h-full" style={{'direction': 'rtl'}}>
+  return <div className="grid grid-rows-32 grid-cols-8 gap-[0.1rem] 2xl:gap-[0.3rem] place-content-around w-fit h-full" style={{'direction': 'rtl'}}>
     {Array(COLS * ROWS).fill(0).map((col, idx) => {
       const revIdx = COLS * ROWS - 1 - idx;
 
-      return <div key={idx} className={cn('bg-black opacity-10 rounded-sm w-5 h-5', {
+      return <div key={idx} className={cn('bg-black opacity-10 rounded-sm w-4 h-4 2xl:h-5 2xl:w-5', {
         '!bg-score !opacity-100': revIdx < Math.pow(2, power)
       })}>&nbsp;</div>;
     })}
