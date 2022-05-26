de0c4951a51ef0a6d398fac84368c5eceee941c7
a.yunyshev <a.yunyshev@corp.vk.com>
2022-05-27 02:44:43 +0700

    Fix TabsSize

--- www/src/components/Tabs/TabButton.tsx
+++ www/src/components/Tabs/TabButton.tsx
@@ -9,7 +9,7 @@ export interface Props extends BaseProps {
 
 const TabButton: FunctionComponent<Props> = ({ active, className, ...baseProps }) =>
   <Button
-    className={cn('transition-spacing max-w-max h-12 2xl:h-16 px-12 text-h7 2xl:text-h5 first:rounded-l-lg last:rounded-r-lg text-active', className, {
+    className={cn('transition-spacing max-w-max h-12 2xl:h-16 px-12 text-h8 2xl:text-h7 first:rounded-l-lg last:rounded-r-lg text-active', className, {
       '!text-black bg-active h-14 2xl:h-[4.5rem] -mt-1 rounded-lg shadow-lg': active
     })}
     ignoreInnerStyle={true}
