fd29cbf4b3fd09c6b5edf82aa1a3c79a9eb0c85d
lapinosore <gudkovpavel@icloud.com>
2022-05-28 09:21:39 +0700

    change round time

--- www/src/lib/settings.ts
+++ www/src/lib/settings.ts
@@ -9,6 +9,6 @@ export const SIGNAL_R_HUB = '/game';
 export const USERS_PER_SESSION = 6;
 export const WINNERS_PER_SESSION = 2;
 
-export const ROUND_TIME = (2 * 60 + 30) * 1000;
+export const ROUND_TIME = (1 * 60 + 30) * 1000;
 
 export const MAX_SCORE = 256;
