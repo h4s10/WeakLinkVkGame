1be40063937f0bae3341bc85ba37dd7a1a988c57
lapinosore <gudkovpavel@icloud.com>
2022-05-29 09:33:01 +0700

    added questions methods

--- WeakLinkGame/WeakLinkGame.API/Controllers/QuestionsController.cs
+++ WeakLinkGame/WeakLinkGame.API/Controllers/QuestionsController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WeakLinkGame.API.Services;
 using WeakLinkGame.DataAccessLayer;
+using WeakLinkGame.DataAccessLayer.Dictionaries;
 
 namespace WeakLinkGame.API.Controllers;
 
@@ -38,5 +40,31 @@ public class QuestionsController : ControllerBase
         _context.Questions.Remove(question);
         await _context.SaveChangesAsync();
         return Ok();
+    }
+    
+    [HttpDelete("noanswer")]
+    public async Task<IActionResult> DeleteNew()
+    {
+        var questions = await _context.Questions.Where(x => x.State == QuestionState.New).ToListAsync();
+        if (!questions.Any())
+            return Ok();
+        
+        _context.Questions.RemoveRange(questions);
+        await _context.SaveChangesAsync();
+        return Ok();
+    }
+    
+    [HttpGet("count")]
+    public async Task<IActionResult> Count()
+    {
+        var questions = await _context.Questions.ToListAsync();
+        if (questions.Any())
+            return Ok();
+        
+        return Ok(new
+        {
+            New = questions.Count(x => x.State == QuestionState.New),
+            NotAnswered = questions.Count(x => x.State != QuestionState.New)
+        });
     }
 }
